3e55e60b3e8f8a6b82f44fdaf8924f05ad2fdd72
Rajesh Srinivasa <rajeshaug2024@gmail.com>
2024-12-31 13:12:39 +0530

    new conversations added

--- 02.Variables.sh
+++ 02.Variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-PERSON1 = Ramesh
-PERSON2 = Suresh 
+PERSON1=Ramesh
+PERSON2=Suresh 
 
-echo "$PERSON1 = Hi $PERSON2"
-echo "$PERSON2 = Hello $PERSON1"
-echo "$PERSON1 = How are you doing?"
-echo "$PERSON2 = I am doing good." 
+echo $PERSON1
+
+# echo "$PERSON1:: Hi $PERSON2"
+# echo "$PERSON2:: Hello $PERSON1"
+# echo "$PERSON1:: How are you doing?"
+# echo "$PERSON2:: I am doing good." 
